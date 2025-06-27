6c3a29fb88cdf7dd37cf508669d5a57d3ebe08bf
Ika <rexar.ika@gmail.com>
2025-06-28 02:35:12 +0300

    filter practice

---
+++ src/main/java/com/tictactoe/LogicServlet.java
@@ -0,0 +1,18 @@
+package com.tictactoe;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet(name = "LogicServlet", value = "/logic")
+public class LogicServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int index = 
+    }
+}
